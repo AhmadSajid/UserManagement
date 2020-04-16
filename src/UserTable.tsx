ba27c429541848e67f162fea1b436245838d9935
Ashraf Ali <ashraf.ali1811@gmail.com>
2020-04-16 19:13:37 +0100

    added bootstrap and used github user profile component

---
+++ src/UserTable.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function UserTable() {
+    return (
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile Number</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    );
+}
+
+export default UserTable;
